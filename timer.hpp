a200a9fa72b36c788e6c21f6a9c7b265b654f6af
belfastkeyboard <ryanftoal@gmail.com>
2024-05-24 17:33:46 +0100

    Typo fixes + logic checks >= now instead of only > for time_elapsed()

--- timer.hpp
+++ timer.hpp
@@ -16,7 +16,7 @@ public:
     void set_timer(int64_t time_to_wait)
     {
         this->_time_stamp = std::chrono::high_resolution_clock::now().time_since_epoch();
-        this->_wait_duration =  std::chrono::duration_cast<std::chrono::nanoseconds>(T(time_to_wait));
+        this->_wait_duration = std::chrono::duration_cast<std::chrono::nanoseconds>(T(time_to_wait));
     }
 
     bool time_elapsed()
@@ -26,18 +26,18 @@ public:
         _current_time = std::chrono::high_resolution_clock::now().time_since_epoch();
         _difference = _current_time - this->_time_stamp;
 
-        return (_difference > this->_wait_duration);
+        return (_difference >= this->_wait_duration);
     }
 
     Timer() : 
         _time_stamp(std::chrono::high_resolution_clock::now().time_since_epoch()),
-        _wait_duration( std::chrono::duration_cast<std::chrono::nanoseconds>(T(0)))
+        _wait_duration(std::chrono::duration_cast<std::chrono::nanoseconds>(T(0)))
     {
         static_assert(std::chrono::__is_duration<T>::value, "Timer type must be std::chrono ");
     }
     Timer(int64_t time_to_wait) : 
         _time_stamp(std::chrono::high_resolution_clock::now().time_since_epoch()),
-        _wait_duration( std::chrono::duration_cast<std::chrono::nanoseconds>(T(time_to_wait)))
+        _wait_duration(std::chrono::duration_cast<std::chrono::nanoseconds>(T(time_to_wait)))
     {
         static_assert(std::chrono::__is_duration<T>::value, "Timer type must be std::chrono ");
     }
